9595641b7de27a28485f8c913384e6d7f4645355
Abdul Coşkun <2104160coskun@zuyd.nl>
2024-05-21 12:50:29 +0200

    slaat op in python geheugen met error

--- ml-frontend/request_handlers/inference_handler.py
+++ ml-frontend/request_handlers/inference_handler.py
@@ -1,9 +1,8 @@
 import base64
 from os import environ
 import logging
-import requests
 from flask_socketio import SocketIO, emit
-from flask import request,jsonify
+from flask import request
 
 from logic.image_processor import ImageProcessor
 
@@ -28,29 +27,19 @@ def _on_disconnect():
 
 
 def _process_image(data):
-    print("TEST")
     user_id = data['user_id'] if data['user_id'] != '' else request.sid
     image_blob = data['image_blob']
-    emotion = data['emotion']
-    
-    video_data = base64.b64decode(image_blob)
-    
-    # Save vTESTideo data to a file
-    print(f"{user_id}_{emotion}_recording.webm")
-    with open(f"{user_id}_{emotion}_recording.webm", "wb") as video_file:
-        video_file.write(video_data)
-    
-    _logger.debug(f'Saved video for user {user_id} with emotion {emotion}.')
-    
-    #Process the image if needed
-    #img_processor = _image_processors.get(user_id, ImageProcessor(INFERENCE_INTERVAL))
-    #img_processor.load_image(image_blob)
-    #img_processor.process(user_id)
 
-    #emit('update-data', img_processor.predictions)
+    img_processor = _image_processors.get(user_id, ImageProcessor(INFERENCE_INTERVAL))
+    img_processor.load_image(image_blob)
+    img_processor.process(user_id)
 
-    #return img_processor.get_image_blob()
-def _process_image(data):
+    emit('update-data', img_processor.predictions)
+
+    return img_processor.get_image_blob()
+
+
+def _process_video(data):
     print("TEST")
     user_id = data['user_id'] if data['user_id'] != '' else request.sid
     image_blob = data['image_blob']
@@ -68,10 +57,11 @@ def _process_image(data):
 
 
 def add_as_websocket_handler(socket_io: SocketIO):
-    global _on_connect, _on_disconnect, _process_image
+    global _on_connect, _on_disconnect, _process_image, _process_video
 
     _on_connect = socket_io.on('connect')(_on_connect)
     _on_disconnect = socket_io.on('disconnect')(_on_disconnect)
     _process_image = socket_io.on('process-image')(_process_image)
+    _process_video = socket_io.on('process-video')(_process_video)
     print("adding hand")
     return socket_io
