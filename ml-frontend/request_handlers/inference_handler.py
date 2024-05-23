16270cbfda1fb098be5dc3b8896090531acc3632
Abdul Coşkun <2104160coskun@zuyd.nl>
2024-05-23 11:56:37 +0200

    werkt met numpy array en tijd info

--- ml-frontend/request_handlers/inference_handler.py
+++ ml-frontend/request_handlers/inference_handler.py
@@ -1,7 +1,7 @@
 
 import os
 import time
-
+import numpy as np
 import cv2
 from os import environ
 import logging
@@ -42,15 +42,18 @@ def _process_image(data):
 
     return img_processor.get_image_blob()
 
+def sanitize_timestamp(timestamp):
+    return timestamp.replace("T", "_").replace(":", "-").replace(".", "-")
 
 def _process_video(data):
     print("TEST")
     user_id = data['user_id'] if data['user_id'] != '' else request.sid
     video_blob = data['image_blob']
     emotion = data['emotion']
+    timestamp = sanitize_timestamp(data['timestamp'])
     
     # Save the video Blob to a temporary file
-    video_path = f"{user_id}_{emotion}_recording.webm"
+    video_path = f"{emotion}_{timestamp}_{user_id}_recording.webm"
     with open(video_path, "wb") as video_file:
         video_file.write(video_blob)
     
@@ -64,30 +67,38 @@ def _process_video(data):
         print("Error: Could not open video.")
         return
 
-    allframes = []
+    frames = []
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
             
-        allframes.append(frame)
+        frames.append(frame)
         cv2.imshow('Video Footage', frame)
 
         # Press 'q' to quit the video window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    numpyframes = np.asarray(allframes)
+    #numpyframes = np.asarray(allframes)
     cap.release()
-    try:
-        time.sleep(1)
-    except:
-        pass
 
     cv2.destroyAllWindows()
     os.remove(video_path)
+   
+    video_array = np.array(frames)
     
-
+    npy_path = f"{emotion}_{timestamp}_{user_id}_recording.npy"
+    np.save(npy_path, video_array)
+    print(f"Video frames saved as NumPy array to {npy_path}")
+    
+    # TEMP
+    np_array = np.load(npy_path)
+    if np.array_equal(video_array, np_array):
+        print("Successfully saved")
+    else:
+        print("Something went wrong")
+    # END OF TEMP
 
 
 def add_as_websocket_handler(socket_io: SocketIO):
