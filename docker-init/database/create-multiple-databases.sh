4b9525d13eb62d52cbb6321f81781a6156de856d
Blazoned <31503221+Blazoned@users.noreply.github.com>
2024-12-05 15:17:01 +0100

    Updated multiple database creation method - Added password login - Detached username from database name - Disallowing connection from unassociated user to database (public connection disallowed)

--- docker-init/database/create-multiple-databases.sh
+++ docker-init/database/create-multiple-databases.sh
@@ -2,23 +2,47 @@
 set -e
 set -u
 
-function create_user_and_database() {
-    local database=$1
+local echo_base="[Docker Postgres Init]"
 
-    echo "Creating user and database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
-        CREATE USER $database;
+function create_user_and_database() {
+    local input=$1
+
+    if [[ "$input" != *\[*\]@* ]]; then
+        echo "$echo_base Error: Invalid input format '${input}'. Must be 'user[password]@database'."
+        exit 1
+    fi
+
+    local user_and_password="${input%@*}"
+    local database="${input#*@}"
+    local user="${user_and_password%\[*\]}"
+    local password="${user_and_password#*[}"
+    password="${password%\]}"
+
+    echo "$echo_base Creating database '$database' and user '$user' with password '$password'"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=postgres <<-EOSQL
+        \set QUIET on
+        CREATE USER $user WITH PASSWORD '$password';
         CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+        REVOKE CONNECT, TEMPORARY ON DATABASE $database FROM PUBLIC;
+        GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
+        \set QUIET off
 EOSQL
 }
 
+echo "$echo_base Revoking public privileges to postgres databases"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=postgres <<-EOSQL
+    \set QUIET on
+    REVOKE CONNECT, TEMPORARY ON DATABASE postgres FROM PUBLIC;
+    \set QUIET off
+EOSQL
+
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-    echo "Creating multiple databases: $POSTGRES_MULTIPLE_DATABASES"
+    echo "$echo_base Creating multiple databases: $(echo $POSTGRES_MULTIPLE_DATABASES | tr ' ' ', ')"
     
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+    for db in $POSTGRES_MULTIPLE_DATABASES; do
         create_user_and_database $db
     done
     
-    echo "Databases created."
+    echo "$echo_base Databases created."
 fi
